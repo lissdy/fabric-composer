0f6d68648cea420fec6d480e446fe78f660219f0
lissdy <445957307@qq.com>
2018-01-11 11:14:48 +0800

    init composer network

---
+++ composer-chaincode/README.md
@@ -0,0 +1,1 @@
+# org.lisa.hyperledger
---
+++ composer-chaincode/lib/logic.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * Write your transction processor functions here
+ */
+
+/**
+ * Sample transaction
+ * @param {org.lisa.hyperledger.ChangeAssetValue} changeAssetValue
+ * @transaction
+ */
+function onChangeAssetValue(changeAssetValue) {
+    var assetRegistry;
+    var id = changeAssetValue.relatedAsset.assetId;
+    return getAssetRegistry('org.lisa.hyperledger.SampleAsset')
+        .then(function(ar) {
+            assetRegistry = ar;
+            return assetRegistry.get(id);
+        })
+        .then(function(asset) {
+            asset.value = changeAssetValue.newValue;
+            return assetRegistry.update(asset);
+        });
+}
